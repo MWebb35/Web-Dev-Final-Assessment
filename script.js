ea8446263cb8f4468cda212f3c15d357d42a9965
MWebb35 <115995439+MWebb35@users.noreply.github.com>
2023-05-01 16:33:39 +0100

    Adds telephone capability, Adds new html files

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function topFunction() {
 function validateReview() {
   var review = document.forms["reviewForm"]["firstName"].value;
   if (review == ""){
-    alert("Please enter your first name out to continue")
+    alert("Please enter your first name out to continue");
     return false;
   }
 }
