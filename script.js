bdfdbfd243c833e14599828f1ba2623c1017eabf
MWebb35 <115995439+MWebb35@users.noreply.github.com>
2023-05-26 13:40:28 +0100

    Adds video functionality

--- script.js
+++ script.js
@@ -58,6 +58,9 @@ function plusSlides(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("slides");
+  if(slides.length == 0) return;
+
+  
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
